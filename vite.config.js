42003eca16074ef574265db2bfc59b2ece35a865
kidehae <149243635+kidehae@users.noreply.github.com>
2025-06-27 13:20:27 +0300

    trying to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+    chunkSizeWarningLimit: 1000, // Adjust based on your needs
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split vendor chunks
+          react: ["react", "react-dom"],
+          amazon: ["@aws-amplify/auth"], // Add other large deps
+        },
+      },
+    },
+  },
+  base: "/",
+});
